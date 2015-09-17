5f6f6b05e449502f19bd1113c54fbdf8923fe948
Andy Situ <and.situ@gmail.com>
2015-09-17 16:19:49 -0700

    added day

--- files/day.js
+++ files/day.js
@@ -3,8 +3,13 @@ function Day(year, month, date, content) {
   this.month = month;
   this.year = year;
   this.instance = this.getInstance(year, month, date);
+  this.day = this.getDay();
 }
 
 Day.prototype.getInstance = function(year, month, date) {
   return new Date(year, month, date);
 }
+
+Day.prototype.getDay = function() {
+  return this.instance.getDay();
+};
