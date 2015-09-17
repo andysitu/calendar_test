36f7a24b148a2f950df02d763e9fa36be41fd18e
Andy Situ <and.situ@gmail.com>
2015-09-17 16:59:01 -0700

    month.makeDays() & properties

--- files/month.js
+++ files/month.js
@@ -1,3 +1,17 @@
-function Month() {
-
+function Month(year, month) {
+  var instance = new Date(year, month);
+  this.year = instance.getFullYear();
+  this.month = instance.getMonth();
+  this.days = [];
 }
+
+Month.prototype.makeDays = function() {
+  for (var date = 1 ; date <= 32 ; date++ ) {
+    var day = new Day(this.year, this.month, date);
+    if (day.month === this.month) {
+      this.days.push(day); 
+    } else {
+      break;
+    }
+  }
+};
