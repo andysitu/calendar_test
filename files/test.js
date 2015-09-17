8a72b7d7cc335a62ae880a806d7c44e560fd7776
Andy Situ <and.situ@gmail.com>
2015-09-17 16:29:09 -0700

    test logs obj if failed

--- files/test.js
+++ files/test.js
@@ -2,10 +2,10 @@ function tester() {
   var test = new Day(2015, 8, 17);
   var assertion = test.date === 17 && test.instance.getDate() === 17
     && test.day === 4;
-  console.assert(assertion);
+  console.assert(assertion, test);
 
   var nextDay = test.makeNextDay();
   assertion = nextDay.date === 18 && nextDay.day === 5;
-  console.assert(assertion);
+  console.assert(assertion, nextDay);
 }
 
