69e2cd3514a746737087cf0e95ceceba6fbd89b7
Andy Situ <and.situ@gmail.com>
2015-09-17 16:06:19 -0700

    Added property to day constructor

--- files/day.js
+++ files/day.js
@@ -1,2 +1,10 @@
 function Day(date, month, year) {
+  this.date = date;
+  this.month = month;
+  this.year = year;
+  this.dateInstance = this.getInstance(date, month, year);
+}
+
+Day.prototype.getInstance = function(date, month, year) {
+  return new Date(year, month, date);
 }
