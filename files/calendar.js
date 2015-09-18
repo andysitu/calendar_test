104b75600cfda3adee16a64d32bdf72fce6a6684
Andy Situ <and.situ@gmail.com>
2015-09-17 17:23:40 -0700

    Calendar - init & makeYear()

--- files/calendar.js
+++ files/calendar.js
@@ -1,3 +1,14 @@
 function Calendar() {
-
+  this.init();
+  this.years = {};
 }
+
+Calendar.prototype.init = function() {
+  var today = new Date();
+  var year = today.getFullYear();
+  this.makeYear(year);
+};
+
+Calendar.prototype.makeYear = function(year) {
+  this.years[year] = new Year(year);
+};
