6ea7718f6cb91644b0b36d5ec569878ce13d3a3d
Andy Situ <and.situ@gmail.com>
2015-09-18 15:56:09 -0700

    changeElement -> makeElement & delete extra copy

--- files/elementfunctions.js
+++ files/elementfunctions.js
@@ -10,7 +10,7 @@ var elementFunctions = {
     }
   },
 
-  createElement: function(type, id) {
+  makeElement: function(type, id) {
   // Returns an element and appends an id.
     if (id === undefined)
       id = "";
@@ -22,17 +22,6 @@ var elementFunctions = {
   getElement: function(id) {
     var element = document.getElementById(id);
     return element;
-  },
-
-  addHandler: function(type, event, handler) {
-  // Adds an event handler to document by type either
-  //  being the id or the element.
-    if (typeof type == 'string') { // element Id
-      var element = document.getElementById(type);
-      element.addEventListener(event, handler);
-    } else if (typeof type == 'object') { // element node
-      type.addEventListener(event, handler);
-    }
   },
 
   append: function(parent, child) {
