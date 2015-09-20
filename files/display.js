1fcab66f44bf341147f01cd63e25db5beeec9265
Andy Situ <and.situ@gmail.com>
2015-09-20 12:28:38 -0700

    displayMonth test if calendarTable exist already

--- files/display.js
+++ files/display.js
@@ -1,6 +1,11 @@
 var display = {
   displayMonth(tableFrag) {
+    var test = document.getElementById("calendarTable");
     var tableDiv = document.getElementById("tableDiv");
-    tableDiv.appendChild(tableFrag);
+    if (test === null) {
+      tableDiv.appendChild(tableFrag);
+    } else {
+      tableDiv.replaceChild(tableFrag, test);
+    }
   }
 };
