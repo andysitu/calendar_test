b8b20ed694cf0752ade4d22000ad6421bf229774
Andy Situ <and.situ@gmail.com>
2015-09-20 17:40:47 -0700

    prevMonth dummy handler

--- files/ui.js
+++ files/ui.js
@@ -1,9 +1,16 @@
 var ui = {
   run: function() {
     eleFunctions.addHandler("nextMonth", "click", this.nextMonth);
+    eleFunctions.addHandler("prevMonth", "click", this.prevMonth);
   },
 
-  nextMonth: function() {
+  nextMonth: function(e) {
     console.log("You clicked on next");
+  },
+  prevMonth: function(e) {
+    console.log("You clicked on previous");
+  },
+  tableClick: function(e) {
+    console.log("You clicked on the calendar", e.target);
   }
 };
