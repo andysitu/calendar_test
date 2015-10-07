7a32ed7cec746b4ecedd34f2d503ceddc5b1d15f
Andy Situ <and.situ@gmail.com>
2015-10-06 18:04:17 -0700

    menu.js - methods taken mainly from display

---
+++ files/menu.js
@@ -0,0 +1,31 @@
+var menu = {
+   _menuStatus: false,
+   createInfoMenu(message) {
+    var menu = this.createMenu("infoMenu");
+    var div = eF.makeElement("div", {}, message, eF.makeElement("input", {type: "text"}), eF.makeElement("input", {type: "submit"}));
+
+    menu.appendChild(div);
+    display.addMenu(menu);
+  },
+  removeInfoMenu() {
+    this.removeMenu("infoMenu");
+  },
+  
+  createMenu(id) {
+    if (this._menuStatus == true) {
+      this.removeMenu(id);
+    }
+    var menu = eF.makeElement("div", {
+      class: "menu",
+      id: id
+    });
+    this._menuStatus = true;
+    return menu;
+  },
+  removeMenu(id) {
+    if (this._menuStatus == true) {
+      display.removeMenu(id);
+      this._menuStatus = false;
+    }
+  }
+};
