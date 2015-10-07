f141574625c01591ea13696e483fdb3e0c0ab381
Andy Situ <and.situ@gmail.com>
2015-10-06 18:05:00 -0700

    disp - remove methods used for menu

--- files/display.js
+++ files/display.js
@@ -27,37 +27,11 @@ var display = {
   addMenu(menu) {
     document.body.appendChild(menu);
   },
-  delMenu(id) {
+  removeMenu(id) {
     var menu = document.getElementById(id);
     document.body.removeChild(menu);
   },
-  createInfoMenu(message) {
-    var menu = this.createMenu("infoMenu");
-    var div = eF.makeElement("div", {}, message, eF.makeElement("input", {type: "text"}), eF.makeElement("input", {type: "submit"}));
-
-    menu.appendChild(div);
-    this.addMenu(menu);
-  },
-  removeInfoMenu() {
-    this.removeMenu("infoMenu");
-  },
-  createMenu(id) {
-    if (this._menuStatus == true) {
-      this.removeMenu(id);
-    }
-    var menu = eF.makeElement("div", {
-      class: "menu",
-      id: id
-    });
-    this._menuStatus = true;
-    return menu;
-  },
-  removeMenu(id) {
-    if (this._menuStatus == true) {
-      this.delMenu(id);
-      this._menuStatus = false;
-    }
-  },
+  
   monthTranslator(month) {
     var months = ["January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"];
