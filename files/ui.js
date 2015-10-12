1a9c25ad1ceb073d989dc8ed8d51a584197082e4
Andy Situ <and.situ@gmail.com>
2015-10-11 20:42:21 -0700

    value, not textContent, for text input

--- files/ui.js
+++ files/ui.js
@@ -34,6 +34,6 @@ var ui = {
   submitInfo: function() {
     var button = document.getElementById("submitInfoButton");
     var text = document.getElementById("info_menu_text");
-    button.addEventListener("click", function(e) { console.log(text.textContent); text.textContent = ""; })
+    button.addEventListener("click", function(e) { console.log(text.value); text.value = ""; })
   }
 };
